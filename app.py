c7d91a89b37a20fa8f43f6cae6afb7c514fd6637
Roselma Mendes <roselma.mendes@gmail.com>
2017-10-04 16:23:24 -0300

    conserta flask no heroku

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-app.run(debug=True, port=int(os.environ.get('PORT')))
+app.run(debug=True, port=int(os.environ.get('PORT')), host='0.0.0.0')
